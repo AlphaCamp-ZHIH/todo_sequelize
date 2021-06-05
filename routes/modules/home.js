83745d6cc0e7468a207a71d822357d9c3b0471f6
zhih <fufong79570@gmail.com>
2021-06-05 21:09:45 +0800

    Render page of index

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-
-res.render('index')
+const db = require("../../models");
+const User = db.User;
+const Todo = db.Todo;
+router.get("/", (req, res) => {
+  const UserId = req.user.id;
+  return Todo.findAll({
+    raw: true,
+    nest: true,
+    where: { UserId },
+  }).then((todos) => {
+    console.log(todos);
+    const notFinished =todos.filter(todo => todo.isDone === 0);
+    todos = todos.filter(todo => todo.isDone === 1)
+    return res.render("index", { todos ,notFinished})});
 });
-
 
 module.exports = router;
